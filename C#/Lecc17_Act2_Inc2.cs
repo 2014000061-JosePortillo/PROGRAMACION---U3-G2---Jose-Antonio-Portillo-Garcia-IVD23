fc4d382d16773bbad36d36e88bc17bb89a7cffab
2014000061-JosePortillo <j.portillo.5adb@gmail.com>
2026-07-02 19:14:11 -0600

    Add files via upload

---
+++ C#/Lecc17_Act2_Inc2.cs
@@ -0,0 +1,28 @@
+﻿internal class Lecc17_Act2_Inc2
+{
+    private static void Main(string[] args)
+    {
+        int numeroFila;
+        int numeroColumna;
+        int[,] matrizColumnas = new int[4,4];
+
+        for (numeroFila = 0; numeroFila < 4; numeroFila++)
+        {
+            for (numeroColumna = 0; numeroColumna < 4; numeroColumna++)
+            {
+                matrizColumnas[numeroFila, numeroColumna] = numeroColumna + 1;
+            }
+        }
+
+        Console.WriteLine("Matriz de Columnas");
+        for(numeroFila = 0; numeroFila < 4; numeroFila++)
+        {
+            for (numeroColumna = 0; numeroColumna < 4; numeroColumna++)
+            {
+                Console.Write(matrizColumnas[numeroFila, numeroColumna] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
---
+++ C#/Lecc17_Act3_Inc1.cs
@@ -0,0 +1,22 @@
+﻿internal class Lecc17_Act3_Inc1
+{
+    private static void Main(string[] args)
+    {
+        int fila, columna,num,suma = 0;
+        int[,] tabla = new int[4, 3];
+
+        for (fila = 0; fila < 4; fila++)
+        {
+            suma = 0;
+            for (columna = 0; columna < 3; columna++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                num = int.Parse(Console.ReadLine());
+                tabla[fila, columna] = num;
+                suma = suma + num;
+            }
+            Console.WriteLine("La suma de la fila "+fila+" es: "+suma);
+        }
+        Console.ReadKey();
+    }
+}
